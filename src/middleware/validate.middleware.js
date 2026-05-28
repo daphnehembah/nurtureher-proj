341a0ffa950a3f34ea5b3398a48448473e0bf39a
daphnehembah <daphnehembah@gmail.com>
2026-05-28 20:52:57 +0100

    added console log to validate middleware

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
 module.exports = (req, res, next) => {
+  console.log('validate middleware called, next type:', typeof next);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
